48f79c85bbab858ba95d9ebc5201236ea5cd7351
ll9 <llwierer@web.de>
2018-09-12 22:56:45 +0200

    datagrid start

--- SpatialiteDataTable/Form1.cs
+++ SpatialiteDataTable/Form1.cs
@@ -14,9 +14,24 @@ namespace SpatialiteDataTable
 {
     public partial class Form1 : Form
     {
+        public DataTable DataTable { get; set; } = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = DataTable;
+            LoadDb();
+
+        }
+
+        public void LoadDb()
+        {
+            using (var connection = GetConnection())
+            {
+                var query = "select AsText(Geometry) from test";
+                var adapter = new SQLiteDataAdapter(query, connection);
+                adapter.Fill(DataTable);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
